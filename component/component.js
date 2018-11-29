613746d6ad20bc01262fa7436912ab010f6bffd5
Mathieu Debove <debovema@users.noreply.github.com>
2018-11-29 13:07:16 +0100

    Update to Rancher 2.1

--- component/component.js
+++ component/component.js
@@ -1,26 +1,44 @@
-/* v----- Do not change anything between here
- *       (the DRIVERNAME placeholder will be automatically replaced during build) */
-define('ui/components/node-driver/driver-%%DRIVERNAME%%/component', ['exports', 'ember', 'shared/components/node-driver/driver-%%DRIVERNAME%%/component'], function (exports, _ember, _component) {
-  exports['default'] = _component['default'];
-});
+/*!!!!!!!!!!!Do not change anything between here (the DRIVERNAME placeholder will be automatically replaced at buildtime)!!!!!!!!!!!*/
+import NodeDriver from 'shared/mixins/node-driver';
+ // do not remove LAYOUT, it is replaced at build time with a base64 representation of the template of the hbs template
 
-define('shared/components/node-driver/driver-%%DRIVERNAME%%/component', ['exports', 'ember', 'shared/mixins/node-driver', 'shared/components/node-driver/driver-%%DRIVERNAME%%/template'], function (exports, _ember, _uiMixinsDriver, _template) {
-/* ^--- And here */
+// we do this to avoid converting template to a js file that returns a string and the cors issues that would come along with that
+const LAYOUT;
+/*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
 
-  // You can put embmer object here
-  var computed = Ember.computed;
-  var get = Ember.get;
-  var set = Ember.set;
-  var alias = Ember.computed.alias;
-  var observer = Ember.observer;
 
-/* v----- Do not change anything between here
- *       (the DRIVERNAME placeholder will be automatically replaced during build) */
-  exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
-    layout: _template.default,
-    driverName: '%%DRIVERNAME%%',
-    config: alias('model.%%DRIVERNAME%%Config'),
-/* ^--- And here */
+/*!!!!!!!!!!!GLOBAL CONST START!!!!!!!!!!!*/
+// EMBER API Access - if you need access to any of the Ember API's add them here in the same manner rather then import them via modules, since the dependencies exist in rancher we dont want to expor the modules in the amd def
+const computed     = Ember.computed;
+const get          = Ember.get;
+const set          = Ember.set;
+const alias        = Ember.computed.alias;
+const service      = Ember.inject.service;
+
+const defaultRadix = 10;
+const defaultBase  = 1024;
+/*!!!!!!!!!!!GLOBAL CONST END!!!!!!!!!!!*/
+
+
+
+/*!!!!!!!!!!!DO NOT CHANGE START!!!!!!!!!!!*/
+export default Ember.Component.extend(NodeDriver, {
+  driverName: '%%DRIVERNAME%%',
+  config:     alias('model.%%DRIVERNAME%%Config'),
+  app:        service(),
+
+  init() {
+    // This does on the fly template compiling, if you mess with this :cry:
+    const decodedLayout = window.atob(LAYOUT);
+    const template      = Ember.HTMLBars.compile(decodedLayout, {
+      moduleName: 'nodes/components/driver-%%DRIVERNAME%%/template'
+    });
+    set(this,'layout', template);
+
+    this._super(...arguments);
+
+  },
+  /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
 
     regions: [{
         id: 'par1',
